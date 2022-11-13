b2fd713975927a77e16190b7ab49fe8de1dd112d
Lucas Vencatachellum <lucas.vencatachellum@epitech.eu>
2022-11-13 23:49:42 +0100

    Delete code directory

--- code/neural_networks/mnist/load_data.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-np.save("data/x_train", x_train)
-np.save("data/y_train", y_train)
-np.save("data/x_test", x_test)
-np.save("data/y_test", y_test)
--- code/overfitting/oracle.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-sigma = 0.0
-
-
-def oracle(x):
-    return np.sin(7 * x) + np.cos(15 * x)
